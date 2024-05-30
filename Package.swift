84c65e84f59010f1dd96f231d7c66bd0fe0526ab
Calvin Cheng <calvin@goodnotesapp.com>
2024-05-30 11:56:45 +0800

    build: update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Aho-Corasick"
+  name: "AhoCorasickSwift",
+  products: [
+    .library(
+        name: "AhoCorasickSwift",
+        targets: ["AhoCorasickSwift"]
+    ),
+  ],
+  targets: [
+    .target(name: "AhoCorasickSwift")
+  ]
 )
